63a0e1675fc4d71d50116637a87c20ff247e4b74
Rudra Tewari <rudratewari30@gmail.com>
2025-11-10 19:37:36 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-amber-900 py-2 text-white  ">Hello Rudra</div>
+    </>
+  )
+}
+
+export default App
